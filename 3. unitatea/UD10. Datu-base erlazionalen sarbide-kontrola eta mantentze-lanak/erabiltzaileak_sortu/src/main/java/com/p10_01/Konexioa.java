5e8151ef0eece7eb05fe30c182e288c4ad999564
aKijn2 <mrkinder20041010@gmail.com>
2025-04-07 10:02:48 +0200

    bukatzeko p10.01 erabiltzaileak sortu

--- 3. unitatea/UD10. Datu-base erlazionalen sarbide-kontrola eta mantentze-lanak/erabiltzaileak_sortu/src/main/java/com/p10_01/Konexioa.java
+++ 3. unitatea/UD10. Datu-base erlazionalen sarbide-kontrola eta mantentze-lanak/erabiltzaileak_sortu/src/main/java/com/p10_01/Konexioa.java
@@ -1,32 +1,68 @@
 package com.p10_01;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
 
-public class Konexioa 
-{
+public class Konexioa {
     static final String db_url = "jdbc:mysql://localhost:3306/programazioa";
     static final String erabiltzailea = "root";
     static final String pasahitza = "mysql";
 
+    Scanner sc = new Scanner(System.in);
 
     /**
      * Erabiltzaileak taulan datuak sartzeko metodoa
+     * 
      * @param konexioa Konektatutako datu-basea
      */
-    public void erabiltzaileak_sortu(Connection konexioa) 
-    {
-        try 
-        {
-            java.sql.Statement stmt = konexioa.createStatement();
-            String sql_insert = "INSERT INTO erabiltzaileak" +
-            "(nan, izena, abizena, pasahitza)" +
-            "VALUES (123456789, 'Jon', 'Garcia', '1234')";
-            stmt.executeUpdate(sql_insert);
-            System.out.println("Datuak ondo sartu dira erabiltzaileak taulan");
-            stmt.close();
-        } 
-        catch (Exception e) 
-        {
+    public void erabiltzaileak_sortu(Connection konexioa) {
+        try {
+            System.out.println("Sartu erabiltzailearen nan-a: ");
+            String nan = sc.nextLine();
+
+            System.out.println("Sartu erabiltzailearen izena: ");
+            String izena = sc.nextLine();
+
+            System.out.println("Sartu erabiltzailearen abizena: ");
+            String abizena = sc.nextLine();
+
+            System.out.println("Sartu erabiltzailearen apodoa: ");
+            String apodoa = sc.nextLine();
+
+            String pasahitza;
+            String pasahitzaErrepikatu;
+            do {
+                System.out.println("Sartu erabiltzailearen pasahitza: ");
+                pasahitza = sc.nextLine();
+
+                System.out.println("Erabiltzailearen pasahitza errepikatu: ");
+                pasahitzaErrepikatu = sc.nextLine();
+
+                /**
+                 * Mezua hitzuli kondizioaren arabera
+                 */
+                if (!pasahitza.equals(pasahitzaErrepikatu)) {
+                    System.out.println("Pasahitzak ez dira berdinak. Saiatu berriro.");
+                } else {
+                    System.out.println("Pasahitzak berdinak dira.");
+                }
+
+            } while (!pasahitza.equals(pasahitzaErrepikatu));
+
+            /**
+             * SQL irakurketa: erabiltzaileak taulan datuak sartzeko
+             */
+            String query = "INSERT INTO erabiltzaileak (nan, izena, abizena, erabiltzailea, pasahitza) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = konexioa.prepareStatement(query);
+            preparedStatement.setString(1, nan);
+            preparedStatement.setString(2, izena);
+            preparedStatement.setString(3, abizena);
+            preparedStatement.setString(4, apodoa);
+            preparedStatement.setString(5, pasahitza);
+            preparedStatement.executeUpdate();
+            System.out.println("Erabiltzailea sortu da.");
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
