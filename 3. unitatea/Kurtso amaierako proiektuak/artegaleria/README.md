14c4ca4150aafe8cc727c2b9571dd007d9ddab89
aKijn2 <mrkinder20041010@gmail.com>
2025-04-30 13:16:16 +0200

    eguneraketa

---
+++ 3. unitatea/Kurtso amaierako proiektuak/artegaleria/README.md
@@ -0,0 +1,4 @@
+## kudeaketa sistema:
+https://excalidraw.com/#room=79a489f4d12c1e7ffe1b,XsXjT0DLczhlwMpch-07VA
+
+![alt text](img/image.png)
---
+++ 3. unitatea/Kurtso amaierako proiektuak/artegaleria/src/main/java/com/artegaleria/Main.java
@@ -0,0 +1,7 @@
+package com.artegaleria;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
